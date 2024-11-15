a5582d862d020555c23811d55b5d57657eee6125
pichaachuu <nafisahaliyah01@gmail.com>
2024-11-15 09:58:58 +0700

    Jobsheet 9

--- ArrayBilangan13.java
+++ ArrayBilangan13.java
@@ -3,18 +3,16 @@ public class ArrayBilangan13 {
     public static void main(String[] args) {
         
         
-        double[] bil = new double[3];
+        double[] bil = new double[4];
 
         bil[0] = 5.0;
         bil[1] = 12867;
         bil[2] = 7.5;
         bil[3] = 2000000;
 
-        System.out.println(bil[0]);
-        System.out.println(bil[1]);
-        System.out.println(bil[2]);
-        System.out.println(bil[3]);
-
+        for ( int i = 0; i <= 4; i++) {
+            System.out.println(bil[i]);
+        }
         
     }
 }
