a24b58ea8d83e9281b61f682d6c8a800f25b3922
pichaachuu <nafisahaliyah01@gmail.com>
2024-11-15 16:48:51 +0700

    Jobsheet 9

--- SearchNilai13.java
+++ SearchNilai13.java
@@ -3,19 +3,38 @@ public class SearchNilai13 {
     public static void main(String[] args) {
         Scanner input13 = new Scanner(System.in);
 
-        int[] arrNilai = {80,85,78,96,90,82,86};
-        int key = 90;
-        int hasil = 0;
+        int jumlahMHS;
+        int key;
+        boolean found = false;
+        
+        System.out.print("Masukkan banyaknya nilai yang akan diinput : ");
+        jumlahMHS = input13.nextInt();
+        
+        int[] arrNilai = new int[jumlahMHS];
+
+        for (int i = 0; i < arrNilai.length; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i+1) + ":");
+            arrNilai[i] = input13.nextInt();
+        }
+
+        System.out.print("Masukkan nilai yang ingin dicari :");
+        key = input13.nextInt();
 
         for (int i = 0; i < arrNilai.length; i++) {
             if (key == arrNilai[i]) {
-                hasil = i;
+                found = true;
+                System.out.println("\nNilai " + key + " ketemu, merupakan nilai mahasiswa ke-" + i );
                 break;
             }
         }
-        System.out.println();
-        System.out.println("Nilai " + key + " ketemu di indeks ke-" + hasil);
-        System.out.println();
+        if (!found) {
+            System.out.println("Nilai yang dicari tidak ditemukan");
+
+        }
+
+        // System.out.println();
+        // System.out.println("Nilai " + key + " ketemu di indeks ke-" + hasil);
+        // System.out.println();
         input13.close();
     }
 }
